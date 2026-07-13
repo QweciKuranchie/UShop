29783e1a34128eb86b3f837db59c7fa91c0de0d4
Richard Nuhu <168305616+QweciKuranchie@users.noreply.github.com>
2026-07-13 05:21:39 +0000

    fix: stack product prices vertically on mobile viewports

--- components/PriceView.tsx
+++ components/PriceView.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const PriceView = ({ price, discount, className }: Props) => {
   return (
-    <div className='flex items-center gap-2'>
+    <div className='flex flex-col sm:flex-row sm:items-center gap-1'>
       <PriceFormatter amount={price} className='text-ushop-purple'/>
       {price && discount && <PriceFormatter amount={price + (discount * price)/100} className='text-ushop-light-text font-normal line-through'/>}
     </div>
