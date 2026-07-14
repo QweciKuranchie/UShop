c43f992a202a84e02ccdf7aa6f32d6e28e5febb4
Richard Nuhu <168305616+QweciKuranchie@users.noreply.github.com>
2026-07-14 04:47:49 +0000

    chore: changed HomeCategories component title to 2xl

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 const HomeCategories = ({categories}:{categories:Category[]}) => {
   return (
     <div className='bg-white border border-ushop-pink/20 my-10 md:my-20 p-5 lg:p-7 rounded-md'>
-        <Title className='border-b pb-3'>Popular Categories</Title>
+        <Title className='border-b pb-3 text-2xl'>Popular Categories</Title>
         <div className='flex flex-wrap gap-5 mt-5 grid grid-cols-1 md:grid-cols-2 lg
         lg:grid-cols-3'>
             {categories.map((category) => (
