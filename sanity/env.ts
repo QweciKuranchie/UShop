0ac6d9c49c5d195d11989759c4705362908459b6
Richard Nuhu <168305616+QweciKuranchie@users.noreply.github.com>
2026-08-18 12:08:30 +0000

    fix: default dataset to 'production' in sanity/env.ts when NEXT_PUBLIC_SANITY_DATASET is not set

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,21 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-07-07'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-07-07';
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  process.env.SANITY_PROJECT_ID ||
+  assertValue(
+    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID (or SANITY_PROJECT_ID)'
+  );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
+  if (!v) {
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
